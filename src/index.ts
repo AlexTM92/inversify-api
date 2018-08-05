5ca38746d6d9610a662f9c9c634481139e3b8165
Alessio Trovato Monastra <alessio.trovatomonastra@gmail.com>
2018-08-05 12:33:15 +0200

    setup projectn structure

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { asd } from './asd';
+// tslint:disable-next-line:no-console
+console.log('ts started!!!');
+// tslint:disable-next-line:no-console
+console.log('ajeje');
+const ajeje = 2;
+console.log(ajeje);
+console.log(asd);
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var fs = require('fs');
+var nodeModules = {};
+fs.readdirSync('./node_modules')
+  .filter(x => {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(mod => {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index.bundle.js'
+  },
+  resolve: {
+    extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader'
+      }
+    ]
+  },
+  target: 'node',
+  externals: nodeModules
+};
