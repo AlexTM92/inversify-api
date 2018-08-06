46e3ac47a93ac4a8d9a503c570482ecb1c3677be
Alessio Trovato Monastra <alessio.trovatomonastra@gmail.com>
2018-08-06 23:04:20 +0200

    completed IRepository

--- src/interfaces/IRepository.ts
+++ src/interfaces/IRepository.ts
@@ -3,4 +3,7 @@ import { IEntity } from './IEntity';
 export interface IRepository<T extends IEntity> {
   get(): T[];
   getById(id: number): T;
+  add(entity: T): number;
+  edit(id: number, entity: T): T;
+  delete(id: number): T;
 }
